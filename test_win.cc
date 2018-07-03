e14bbcc47ed2e17b1c0b8319c84b55b63e936049
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2018-07-03 15:52:23 +0300

    Removed test win2 function

--- test_win.cc
+++ test_win.cc
@@ -7,6 +7,8 @@
 
 #include "util.h"
 
+using namespace std;
+
 int main() {
     int h[5] = {0,1,2,3,4};
     clock_t start, end;
@@ -27,15 +29,20 @@ int main() {
     printf("%d hands in %f seconds, win sum %I64d\n", I, (double)(end - start)/CLOCKS_PER_SEC, wins);
     */
 
+    //map<int,int> C;
+
     wins = 0; I = 0;
     start = clock();
-    for(int i=0; i<10; i++) {
+    for(int i=0; i<1; i++) {
         for(int i=0; i<5; i++) h[i] = i;
         do {
+            //C[win(h)]++;
             wins += win(h);
             I++;
         } while(next_combi(h, 5, 53-1));
     }
     end = clock();
     printf("%d hands in %f seconds, win sum %I64d (should be 720462 per round)\n", I, (double)(end - start)/CLOCKS_PER_SEC, wins);
+
+    //for(auto p : C) cout << p.first << " x " << p.second << endl;
 }
